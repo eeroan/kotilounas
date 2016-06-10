10e12f33908de3a2c48717876751627120c2d297
Eero Anttila <eea@iki.fi>
2016-06-10 13:10:35 +0300

    Strip html tags and cleanup css

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const head = `<!DOCTYPE html>
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <style>
-* {font-size:14px; font-family: Arial;text-align:left !important;margin:0; padding:0;}
-table, tr, th, td {display:block !important;}
-body {margin: 1em; color: #333;}
-.title {font-size:35px; margin: 2em 0 1em;}
-
+body {font:14px Arial; margin: 1em; color: #333; line-height:1.3;}
+h1, h2 {margin: 1em 0 .5em; font-weight: normal}
+h1 {font-size:35px; }
+h2 {font-size:25px; }
 </style>
 </head>
-<body>`
+<body>
+<h1>Vanhankaupunginkosken lounaslistat</h1>`
 const startMsg = `Server started in http://localhost:${port}, took `
 console.time(startMsg)
 http.createServer((req, res) => {
@@ -27,8 +27,8 @@ http.createServer((req, res) => {
             kahvitupa:   'http://kahvitupa.net/index.php?p=1_3'
         }, bodies => {
             res.end(`${head}
-        <div class="title">Kahvitupa</div>${mapKahvitupa(bodies.kahvitupa)}
-        <div class="title">Koskenranta</div>${mapKoskenranta(bodies.koskenranta)}
+        <h2>Kahvitupa</h2>${mapKahvitupa(bodies.kahvitupa)}
+        <h2>Koskenranta</h2>${mapKoskenranta(bodies.koskenranta)}
         </body></html>`)
         })
     } else {
@@ -37,11 +37,16 @@ http.createServer((req, res) => {
     }
 }).listen(port, () => console.timeEnd(startMsg))
 
-const mapKoskenranta = str => str.replace(/\n/g, '').match(/(LOUNAS vko.*)<img width="300" height="169"/)[1]
+const weekdays = ['maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai'].map(x=>x.toUpperCase())
+const separateWeekDays = strs => strs.map(x=> x.match(new RegExp(`(${weekdays.join('|')})`, 'g')) ? `<br><strong>${x}</strong>` : x).join('<br>')
+const stripTags = str => str.replace(/<[^>]+>/g, 'DIVIDER').split('DIVIDER')
+    .map(x => x.replace(/&nbsp;/g, '')).filter(x=>x.trim().length)
 
-const mapKahvitupa = str => str.replace(/[\n\r]+/g, '')
+const mapKoskenranta = str => separateWeekDays(stripTags(str.replace(/[\n|\t]/g, '').match(/(LOUNAS vko.*)<img width="300" height="169"/)[1]))
+
+const mapKahvitupa = str => stripTags(str.replace(/[\n\r]+/g, '')
     .match(/(<table style="width: 830px.*)<img src="images\/footer.jpg/)[1]
-    .replace(/<p>&nbsp;<\/p>/g, '')
+    .replace(/<p>&nbsp;<\/p>/g, '')).join('<br>')
 
 const combineTemplate = (urls, cb) => {
     var names = Object.keys(urls)
