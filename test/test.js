23a7416359c30496b06afb38cd2e30ca4d9f1f22
Eero Anttila <eea@iki.fi>
2016-06-11 13:43:05 +0300

    Write utility method for tests

--- test/test.js
+++ test/test.js
@@ -4,14 +4,17 @@ const fs = require('fs')
 
 const readFile = path => fs.readFileSync(`${__dirname}/${path}`, {encoding: 'utf8'})
 const normalize = str => str.replace(/\n/g, '')
-
+const equal = (msg, expected, actual) => {
+    console.log('- ' + msg)
+    assert.equal(expected, actual)
+}
 console.log('\033[33mRunning tests...\033[39m')
 
-assert.equal(
+equal('Kahvitupa result is correct',
     parser.mapKahvitupa(readFile('kahvitupa.html')),
     normalize(readFile('expectedKahvitupa.txt'))
 )
-assert.equal(
+equal('Koskenranta result is correct',
     parser.mapKoskenranta(readFile('koskenranta.html')),
     normalize(readFile('expectedKoskenranta.txt'))
 )
