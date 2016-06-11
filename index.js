4c26b052feea78487ce2d4af65e8debc819f78d5
Eero Anttila <eea@iki.fi>
2016-06-11 13:51:27 +0300

    Use top-down order

--- index.js
+++ index.js
@@ -2,36 +2,10 @@ const http = require('http')
 const url = require('url')
 const {mapKahvitupa, mapKoskenranta} = require('./parser')
 const port = process.env.PORT || 5000
-const head = `<!DOCTYPE html>
-<html>
-<head>
-<title>Kotilounas</title>
-<meta name="apple-mobile-web-app-capable" content="yes"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<style>
-body {font:1em Arial; margin: 0; color: #333; line-height:1.3;background: #F0FFF2}
-h1, h2 {margin: 0 0 .5em; font-weight: normal}
-h1 {font:2em Impact; margin: 20px 20px .5em;text-shadow: 0 1px 8px #666; color:#fff;}
-h2 {font-size:1.6em; }
-section { box-shadow: 0px 1px 3px 0px #999; margin:20px; padding:1em; border-radius:3px;background:#fff;}
-</style>
-</head>
-<body>
-<h1>Vanhan&shy;kaupungin&shy;kosken lounaslistat</h1>`
-const startMsg = '\033[33mServer started in \033[36mhttp://localhost:'+ port+', \033[33mtook \033[39m'
-const gaCode = `<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-4154602-9', 'auto');
-  ga('send', 'pageview');
-
-</script>`
+const startMsg = '\033[33mServer started in \033[36mhttp://localhost:' + port + ', \033[33mtook \033[39m'
 console.time(startMsg)
 http.createServer((req, res) => {
-    var uri = url.parse(req.url).pathname
+    const uri = url.parse(req.url).pathname
     if(req.method === 'GET' && uri === '/') {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
         combineTemplate({
@@ -51,19 +25,45 @@ http.createServer((req, res) => {
 }).listen(port, () => console.timeEnd(startMsg))
 
 const combineTemplate = (urls, cb) => {
-    var names = Object.keys(urls)
-    var results = {}
+    const names = Object.keys(urls)
+    const results = {}
     names.forEach(name => get(urls[name], body => {
         results[name] = body
         if(Object.keys(results).length === names.length) cb(results)
     }))
 }
 
-const get = (url, cb) => {
-    http.get(url, res => {
-        var chunks = []
-        res.setEncoding('utf8')
-        res.on('data', chunk => chunks.push(chunk))
-        res.on('end', () => cb(chunks.join('')))
-    })
-}
+const get = (url, cb) => http.get(url, res => {
+    const chunks = []
+    res.setEncoding('utf8')
+    res.on('data', chunk => chunks.push(chunk))
+    res.on('end', () => cb(chunks.join('')))
+})
+
+const head = `<!DOCTYPE html>
+<html>
+<head>
+<title>Kotilounas</title>
+<meta name="apple-mobile-web-app-capable" content="yes"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<style>
+body {font:1em Arial; margin: 0; color: #333; line-height:1.3;background: #F0FFF2}
+h1, h2 {margin: 0 0 .5em; font-weight: normal}
+h1 {font:2em Impact; margin: 20px 20px .5em;text-shadow: 0 1px 8px #666; color:#fff;}
+h2 {font-size:1.6em; }
+section { box-shadow: 0px 1px 3px 0px #999; margin:20px; padding:1em; border-radius:3px;background:#fff;}
+</style>
+</head>
+<body>
+<h1>Vanhan&shy;kaupungin&shy;kosken lounaslistat</h1>`
+
+const gaCode = `<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-4154602-9', 'auto');
+  ga('send', 'pageview');
+
+</script>`
