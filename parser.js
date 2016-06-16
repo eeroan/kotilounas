a7d69a491d3c9ade1868ceed03bea3bf06b00be1
Eero Anttila <eea@iki.fi>
2016-06-16 20:23:40 +0300

    Show only upcoming menus

--- parser.js
+++ parser.js
@@ -16,6 +16,7 @@ module.exports = {
 const weekdays = ['sunnuntai', 'maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai'].map(x=>x.toUpperCase())
 const matchWeekday = new RegExp(`(${weekdays.join('|')})`, 'g')
 const separateWeekDays = (strs, currentDate)=> {
+    const currentMidnight = toMidnight(currentDate)
     return strs.reduce((days, line) => {
         if(line.match(matchWeekday))
             days.push({date: strToDate(line.split(' ')[0]), markup: [`<br><strong>${line}</strong>`]})
@@ -23,7 +24,7 @@ const separateWeekDays = (strs, currentDate)=> {
             days[days.length - 1].markup.push(line)
         return days
     }, [])
-        .filter(({date}) => date >= toMidnight(currentDate))
+        .filter(({date}) => date >= currentMidnight)
         .map(({markup}) => markup.join('<br>\n'))
         .join('<br>\n')
 }
@@ -32,12 +33,13 @@ const stripTags = str => str.replace(/<[^>]+>/g, 'DIVIDER').split('DIVIDER')
 
 const strToDate = str => {
     const dmy = str.split('.')
-    return new Date(+dmy[2], +dmy[1], +dmy[0])
+    return new Date(+dmy[2], (+dmy[1]-1), +dmy[0])
 }
 
 const toMidnight = date => {
     date.setHours(0)
     date.setMinutes(0)
+    date.setSeconds(0)
     date.setMilliseconds(0)
     return date
 }
